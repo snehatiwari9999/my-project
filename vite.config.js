f00f7d34f3dc677ef7ebc009f1d8766c59487c54
anjeetpatel <anjeetpatel0306@gmail.com>
2025-02-16 20:23:33 +0530

    fixing

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,7 @@
 import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  root: ".", // Explicitly set the root directory
-  plugins: [react()],
   build: {
-    outDir: "dist",
-    rollupOptions: {
-      input: "./index.html", // Use a relative path
-    },
+    outDir: "dist", // Ensure this matches Netlify's publish directory
   },
 });
